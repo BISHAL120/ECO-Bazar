cef82fde38fee7725be95cad727f96b777129e94
MD BISHAL <monerulmd5@gmail.com>
2024-03-25 10:41:24 +0600

    minor change

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Wrapper from "@/Components/Wrapper.tsx/Wrapper";
+import Wrapper from "@/Components/Wrapper/Wrapper";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -10,7 +10,7 @@ export default function Home() {
           <div className="flex items-start flex-col md:flex-row gap-2 text-[12px] font-normal leading-5 h-[20px] md:h-[30px] cursor-pointer">
             <div className="md:flex items-center gap-2">
               <Image
-                src="../public/assets/svg/map pin.svg"
+                src="/assets/svg/map pin.svg"
                 width={17}
                 height={20}
                 alt="Location Svg"
